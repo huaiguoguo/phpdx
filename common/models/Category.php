13893f32b4b9bce80bc5633b025e63cc1afb884b
huochaitou <gufe198@163.com>
2016-09-09 02:01:14 +0800

    修改model

--- common/models/Category.php
+++ common/models/Category.php
@@ -29,7 +29,7 @@ class Category extends ActiveRecord
      */
     public static function tableName()
     {
-        return '{{%Category}}';
+        return '{{%category}}';
     }
 
     public function behaviors()
