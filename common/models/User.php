2036256fda9fc04a265718f1d510625251e309e9
huochaitou <gufe198@163.com>
2016-09-28 16:36:36 +0800

    增加手机查找和邮箱查找

--- common/models/User.php
+++ common/models/User.php
@@ -147,6 +147,17 @@ class User extends ActiveRecord implements IdentityInterface
         return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
     }
 
+
+    public static function findByMobile($username)
+    {
+        return static::findOne(['mobile' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    public static function findByEmail($username)
+    {
+        return static::findOne(['email' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+
     /**
      * Finds user by password reset token
      *
