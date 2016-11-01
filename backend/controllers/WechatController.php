e04c1c0c50be80a5bd33e16b41e35498bbb3a419
huochaitou <gufe198@163.com>
2016-11-01 20:53:09 +0800

    调试

--- backend/controllers/WechatController.php
+++ backend/controllers/WechatController.php
@@ -12,17 +12,85 @@ use yii;
 
 class WechatController extends yii\web\Controller
 {
+
+//    public function beforeAction($action)
+//    {
+//        $currentaction  = $action->id;
+//        $novalidactions = ['dologin'];
+//        if (in_array($currentaction, $novalidactions)) {
+//            $action->controller->enableCsrfValidation = false;
+//        }
+//        return parent::beforeAction($action);
+//    }
+
+
     public function actionIndex()
     {
-        $data = [];
-        if ($echostr = $this->check()) {
-            return $echostr;
+        $echoStr = Yii::$app->request->get('echostr');
+        if ($this->checkSignature() && Yii::$app->request->get('echostr')) {
+            return $echoStr;
+        } else {
+            return $this->responseMsg();
         }
-        return $this->render('index', $data);
     }
 
 
-    public function check(){
+    public function responseMsg()
+    {
+        $postAtr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        // 2.处理消息类型
+        /*
+        <xml>
+        <ToUserName><![CDATA[toUser]]></ToUserName>
+        <FromUserName><![CDATA[FromUser]]></FromUserName>
+        <CreateTime>123456789</CreateTime>
+        <MsgType><![CDATA[event]]></MsgType>
+        <Event><![CDATA[subscribe]]></Event>
+        </xml>
+        */
+        $postObj = simplexml_load_string($postAtr);
+//        $postObj->ToUserName = '';
+//        $postObj->FromUserName = '';
+//        $postObj->CreateTime = '';
+//        $postObj->MsgType = '';
+//        $postObj->Event = '';
+        if (strtolower($postObj->MsgType) == 'event') {
+            //如果是关注事件 ：subscribe
+            if ($postObj->Event == 'subscribe') {
+                //回复用户消信息
+                $toUser   = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time     = time();
+                $MsgType  = 'text';
+                $Content  = "欢迎关注";
+                /*
+<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[fromUser]]></FromUserName>
+<CreateTime>12345678</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[你好]]></Content>
+</xml>
+ */
+                $template = "
+                                <xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
+                                </xml>
+                            ";
+                $info = sprintf($template, $toUser, $fromUser, $time, $MsgType, $Content);
+                return $info;
+            }
+        }
+
+    }
+
+
+    public function checkSignature()
+    {
         $signature = Yii::$app->request->get('signature');
         $timestamp = Yii::$app->request->get('timestamp');
         $nonce     = Yii::$app->request->get('nonce');
@@ -32,13 +100,11 @@ class WechatController extends yii\web\Controller
         $tmpstr = implode("", $tmparray);
         $tmpstr = sha1($tmpstr);
         if ($tmpstr == $signature) {
-            $echostr = Yii::$app->request->get('echostr');
-            return $echostr;
-        }else{
+            return true;
+        } else {
             return false;
         }
     }
-
 
 
 }
